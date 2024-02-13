789902ef943dd53345f2d4b822ea78c3019f959d
muhammad-banqusli <your.email@example.com>
2024-02-13 19:53:31 +0300

    Update allowed origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://pdf-text-extractor-ozgq.onrender.com/', 'http://localhost:3000', 'http://localhost:3001'
+    'https://pdf-text-extractor-ozgq.onrender.com', 'http://localhost:3000', 'http://localhost:3001'
 ]
 
 module.exports = allowedOrigins
